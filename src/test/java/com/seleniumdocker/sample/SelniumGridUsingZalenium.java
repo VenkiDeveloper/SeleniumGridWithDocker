21da52415cb4132fff0dbdf76af9e3735059b185
Venkatesh Aarelly <venkatesh.aarelly@citrix.com>
2018-05-24 10:01:23 +0530

    Adding docker files to git.

---
+++ src/test/java/com/seleniumdocker/sample/SelniumGridUsingZalenium.java
@@ -0,0 +1,35 @@
+package com.seleniumdocker.sample;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SelniumGridUsingZalenium {
+	WebDriver driver;
+	@BeforeTest
+    public void setUp() throws MalformedURLException {
+		System.out.println("======> ABC==========>");
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+    }
+
+    @Test
+    public void abc() throws InterruptedException {
+    	System.out.println("======> cbd==========>");
+    	driver.get("http://www.google.com");
+    	Assert.assertTrue(driver.getTitle().contains("Google"));
+    }
+    
+    @AfterTest
+    public void tearDown() throws InterruptedException {
+    	System.out.println("======> ggg==========>");
+        driver.quit();
+    }  
+}
